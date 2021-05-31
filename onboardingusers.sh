435c78a4ab2435f6872065ef29b02749451e050e
oayoade <83650324+oayoade@users.noreply.github.com>
2021-05-31 15:16:56 +0100

    Add files via upload

---
+++ onboardingusers.sh
@@ -0,0 +1,25 @@
+
+# setting variable for filename
+CSV_FILE=names.csv
+PASSWORD=password
+
+
+
+
+#Creating each user
+
+while IFS= read $USERNAME
+do
+#check if username already exists
+If [ $(getent passwd $USERNAME) ];
+then
+echo $USERNAME already exists
+else
+    sudo useradd -m -G developers $USERNAME
+    sudo echo -e "$PASSWORD\n$PASSWORD" | sudo passwd "${USERNAME}""
+    mkdir home/$USERNAME/home/.ssh
+    touch home/$USERNAME/home/.ssh/authorized_keys
+    bash -c "cat .ssh/id_rsa.pub >> home/$USERNAME/.ssh/authorized_keys"
+
+fi
+done < $CSV_FILE    
